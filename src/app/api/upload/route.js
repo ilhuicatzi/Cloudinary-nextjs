84028a560bc85f6d3d5d71ce79996053da821298
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2023-08-31 22:42:23 -0500

    .env.local var defined

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 import {v2 as cloudinary} from 'cloudinary';
           
 cloudinary.config({ 
-  cloud_name: 'dtgxrnxer', 
-  api_key: '879335523621296', 
-  api_secret: 'tzVTq0i_ZmTAo4-rXP4h9xNvHSw' 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 export async function POST(request){
